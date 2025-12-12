c8007ba62f59cb885afa1e932b4e5bf2c75af5f9
Andrew(Shendru) <dev.andrewearl@gmail.com>
2025-12-13 03:23:47 +0800

    Updated User.js Schema

--- models/User.js
+++ models/User.js
@@ -1,20 +1,29 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
   },
+  // === NEW FIELDS ===
+  farmName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: true, // Now Mandatory
+    trim: true,
+  },
+  // ==================
   createdAt: {
     type: Date,
     default: Date.now,
